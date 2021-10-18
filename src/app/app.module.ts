6b57f4fe9cdbc7f07d2f092cfe403577a4395e25
augusto araujo <augusto.aas@gmail.com>
2021-10-18 16:05:59 -0300

    att

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CadastrarProdutosComponent } from './cadastrar-produtos/cadastrar-produtos.component';
+import { ConsultaProdutosComponent } from './consulta-produtos/consulta-produtos.component';
+import {HttpClientModule} from '@angular/common/http';
+
+const appRoutes : Routes = [
+{path : 'cadastro-produtos', component : CadastrarProdutosComponent},
+{path : 'consulta-produtos', component : ConsultaProdutosComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastrarProdutosComponent,
+    ConsultaProdutosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
